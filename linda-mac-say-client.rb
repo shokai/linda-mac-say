7d5c37ae592078a6438767710d6ea5a49e644db5
Sho Hashimoto <hashimoto@shokai.org>
2013-05-23 07:45:14 +0900

    say client

---
+++ linda-mac-say-client.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'sinatra/rocketio/linda/client'
+$stdout.sync = true
+
+url = "http://linda.masuilab.org"
+str = ARGV.shift || "hello"
+
+puts "connecting.. #{url}"
+linda = Sinatra::RocketIO::Linda::Client.new url
+ts = linda.tuplespace["delta"]
+
+linda.io.on :connect do  ## RocketIO's "connect" event
+  puts "connect!! <#{session}>"
+  ts.write ["say", str]
+  exit
+end
+
+loop do
+end
