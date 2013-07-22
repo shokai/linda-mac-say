aa553b72173c52ac58977fee5336271c731bd394
Sho Hashimoto <hashimoto@shokai.org>
2013-07-23 04:23:30 +0900

    use em-rocketio-linda-client

--- linda-mac-say-client.rb
+++ linda-mac-say-client.rb
@@ -1,20 +1,22 @@
 #!/usr/bin/env ruby
 require 'rubygems'
-require 'sinatra/rocketio/linda/client'
+require 'em-rocketio-linda-client'
 $stdout.sync = true
 
 url =   ENV["LINDA_BASE"]  || ARGV.shift || "http://localhost:5000"
-space = ENV["LINDA_SPACE"] || "test"
+puts space = ENV["LINDA_SPACE"] || "test"
 str =   ARGV.shift || "hello"
 
 puts "connecting.. #{url}"
-linda = Sinatra::RocketIO::Linda::Client.new url
-ts = linda.tuplespace[space]
+EM::run do
+  linda = EM::RocketIO::Linda::Client.new url
+  ts = linda.tuplespace[space]
 
-linda.io.on :connect do  ## RocketIO's "connect" event
-  puts "connect!! <#{session}>"
-  ts.write ["say", str]
-  exit
+  linda.io.on :connect do  ## RocketIO's "connect" event
+    puts "connect!! <#{session}>"
+    ts.write ["say", str]
+    EM::add_timer 3 do
+      exit
+    end
+  end
 end
-
-linda.wait
